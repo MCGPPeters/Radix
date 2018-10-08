5714aad11d91600e93144f368ba1bedbeeac419b
Maurice CGP Peters <mcgppeters@hotmail.com>
2018-10-08 09:40:12 +0200

    wip

--- F#/src/Radix/File1.fs
+++ F#/src/Radix/File1.fs
@@ -1,6 +1,8 @@
 ﻿module Root
 
 open System.IO
+open System
+open System.Security.Cryptography
 
 type Undefined = exn
 
@@ -8,16 +10,26 @@ type AsyncResult<'success, ' failure> = Async<Result<'success, 'failure>>
 
 type Hash = Hash of byte[]
 
-type Address = Address of Hash
-
-module Address = 
-    
-    open System.Security.Cryptography
-    open System
-
-    let internal create (guid: Guid) = 
+[<AbstractClass>]
+type Address(guid: Guid) = 
+    member private this.Hash = 
         let sha1 = SHA1.Create()
-        Address (Hash (sha1.ComputeHash(guid.ToByteArray())))
+        Hash (sha1.ComputeHash(guid.ToByteArray()))
+    
+    interface IComparable<Address> with
+        member x.CompareTo(y) = 
+            let (Hash x) = x.Hash
+            let (Hash y) = y.Hash
+            BitConverter.ToString(x).CompareTo(BitConverter.ToString(y))
+
+    interface IComparable with 
+        member x.CompareTo(y) = 
+            let (Hash x) = x.Hash
+            let address: Address = (downcast y) 
+            let (Hash y) = address.Hash
+            BitConverter.ToString(x).CompareTo(BitConverter.ToString(y))
+
+    abstract member Send : 'message -> unit 
 
 type ValidationError = Undefined
 
@@ -30,11 +42,11 @@ type Map<'a, 'b> = Validated<'a> -> 'b
 type Envelope =
     abstract member Address: Address
 
-type Envelope<'message> = {
-    Address : Address
-    Message : 'message
-    }
-    with interface Envelope
+type Envelope<'message>(address, message) = 
+    interface Envelope with 
+        member this.Address: Address = address
+    member this.Message: 'message = message
+            
 
 type DTO<'payload> = {
     Address : Address
@@ -73,20 +85,26 @@ module Routing =
 
     type Agent = Agent of MailboxProcessor<Envelope>
 
-    type Registry = Registry of Map<Address, Agent>
+    type Registry = Registry of FSharp.Collections.Map<Address, Agent>
 
     type UnableToDeliverEnvelopeError = UnableToDeliverEnvelopeError of string
 
     open TrustBoundary
 
     type Forward = Uri -> Envelope -> AsyncResult<EnvelopeForwarded, UnableToDeliverEnvelopeError>
-    
+
+module Actor = 
+
+    type Behavior<'state, 'message> = ^state -> ^message -> ^state
+
+    type Create< ^state, ^message> = Behavior< ^state, ^message> -> ^state -> Address
 
 type BoundedContext = BoundedContext of MailboxProcessor<Envelope>
 
 module BoundedContext =
  
     open Routing
+    open System
 
     let create (resolve : Resolve) (forward: Forward) (Registry registry) =
 
@@ -96,7 +114,13 @@ module BoundedContext =
                     match registry.TryFind envelope.Address with
                     | Some (Agent agent) -> 
                         agent.Post envelope
-                    | _ -> None
+                    | _ -> 
+                        let! resolveResult = resolve envelope.Address
+                        match resolveResult with
+                        | Ok uri-> 
+                            let! forwardResult = forward uri envelope
+                            forwardResult |> ignore // log
+                        | Error error -> error |> ignore //log
                     return! messageLoop (Registry state)
                 }
                     
@@ -119,9 +143,6 @@ module IO =
 
     type Publish<'payload, 'message> = Output<'message, 'payload> -> Envelope<'message> -> unit
 
-
- 
-
 module Operators = 
 
-    let (<--) = 
+    let inline (<--) (address: Address) (message: 'message) = address.Send message
