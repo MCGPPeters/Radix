7ddeee1dd2a67ea8a9083fde5bc87f8595eb6f5e
Maurice CGP Peters <mcgppeters@gmail.com>
2019-02-13 16:36:12 +0100

    implementation of dual numbers

--- F#/src/Radix/Radix.Math.Pure.Numbers.fs
+++ F#/src/Radix/Radix.Math.Pure.Numbers.fs
@@ -12,3 +12,33 @@ type ProperFraction =
             | Interval.Open 0.0 1.0 x -> Ok (ProperFraction x)
             | _ -> Error "n is not a proper fraction"
         static member (*) (ProperFraction a, ProperFraction b) = ProperFraction.Create(a * b)
+
+type Dual = Dual of double * double
+
+module Dual = 
+
+    let ofDouble (d: double) = 
+        Dual (d, 0.0)
+
+    let ofInteger (i: int) = 
+        Dual (double i, 0.0)
+
+    let epsilonOne = Dual (0.0, 1.0)        
+
+    let inline real (Dual (a, _)) = a
+
+    let inline dual (Dual (_, b)) = b
+
+    let inline (/) (Dual (a, b)) (Dual (c, d)) =
+        Dual (a / c, (b * c - d * a) / c * c)
+
+    let inline (*) (Dual (a, b)) (Dual (c, d)) =
+        Dual (a * c, (a * d) + (b * c))
+
+    let inline (+) (Dual (a, b)) (Dual (c, d)) =
+        Dual (a + c, b + d) 
+
+    let inline (-) (Dual (a, b)) (Dual (c, d)) =
+        Dual (a - c, b - d) 
+
+    
