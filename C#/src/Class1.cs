985c671df8561a628ac463614e721526cde5ecc2
Maurice CGP Peters <mcgppeters@gmail.com>
2019-05-01 10:56:05 +0200

    Test the monad laws for the result type

--- C#/src/Class1.cs
+++ C#/src/Class1.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Radix
+﻿namespace Radix
 {
     public class Class1
     {
---
+++ C#/tests/Unit.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Radix.Tests
+{
+    public struct Unit : IComparable<Unit>, IEquatable<Unit>
+    {
+        public static readonly Unit Instance = new Unit();
+
+        public int CompareTo(Unit other)
+        {
+            return 0;
+        }
+
+        public bool Equals(Unit other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "{}";
+        }
+    }
+}
