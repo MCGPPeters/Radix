12cd353df1423cdfd82a7bc21a4a2acb0f15f52b
Maurice CGP Peters <mcgppeters@gmail.com>
2020-03-11 21:46:45 +0100

    NavLink and routing start

---
+++ C#/Radix.Blazor/Html/Components.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace Radix.Blazor.Html
+{
+    public static class Components
+    {
+        public static component navLink(NavLinkMatch navLinkMatch) => (attributes, children) =>
+        {
+
+            attributes.Prepend(new ComponentAttribute("Match", navLinkMatch));
+
+            return component<NavLink>(attributes, children);
+        };
+
+        public static component navLinkMatchAll => (attributes, children) => navLink(NavLinkMatch.All)(attributes, children);
+        public static component navLinkMatchPrefix => (attributes, children) => navLink(NavLinkMatch.Prefix)(attributes, children);
+
+        public static Component component<T>(IEnumerable<IAttribute> attributes, params Node[] children) 
+            where T : IComponent
+        {
+            return new Component(typeof(T), attributes, children);
+        }
+    }
+
+}
