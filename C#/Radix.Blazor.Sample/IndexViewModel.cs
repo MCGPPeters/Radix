14044bb1ad1edd9affe6320d73aebd0762e29f8d
Maurice CGP Peters <mcgppeters@gmail.com>
2020-03-11 17:00:52 +0100

    Listing inventory items, layout, data from viewmodel

--- C#/Radix.Blazor.Sample/IndexViewModel.cs
+++ C#/Radix.Blazor.Sample/IndexViewModel.cs
@@ -1,20 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Radix.Tests.Models;
 
 namespace Radix.Blazor.Sample
-{
+{ 
 
     public class IndexViewModel : State<IndexViewModel, InventoryItemEvent>, IEquatable<IndexViewModel>
     {
-
-        public bool Equals(IndexViewModel other)
+        /// <summary>
+        /// This is just an example.. in real life this would be a database or something
+        /// </summary>
+        private static readonly List<(Address address, string Name)> _inventoryItems = new List<(Address, string)>
         {
-            return true;
-        }
+            (new Address(Guid.NewGuid()), "First item"),
+            (new Address(Guid.NewGuid()), "Second item"),
+        };
+
+        public IEnumerable<(Address, string)> InventoryItems => _inventoryItems;
+
+
 
         public IndexViewModel Apply(InventoryItemEvent @event)
         {
-            throw new NotImplementedException();
+            (Address address, string Name) item;
+            switch (@event)
+            {
+                case InventoryItemCreated inventoryItemCreated:
+                    _inventoryItems.Add((inventoryItemCreated.Address, inventoryItemCreated.Name));
+                    break;
+                case InventoryItemDeactivated inventoryItemDeactivated:
+                    item = _inventoryItems.Find(item => item.address.Equals(inventoryItemDeactivated.Address));
+                    _inventoryItems.Remove(item);
+                    break;
+                case InventoryItemRenamed inventoryItemRenamed:
+                    item = _inventoryItems.Find(item => item.address.Equals(inventoryItemRenamed.Address));
+                    item.Name = inventoryItemRenamed.Name;
+                    break;
+            }
+
+            return this;
+        }
+
+        public bool Equals(IndexViewModel other)
+        {
+            return InventoryItems.SequenceEqual(other.InventoryItems);
         }
     }
 }
