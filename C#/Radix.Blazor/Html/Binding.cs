cd6b58b8c46eeb495235985819d4b79080f33317
Maurice CGP Peters <mcgppeters@gmail.com>
2020-04-08 16:03:45 +0200

    Binding syntax

---
+++ C#/Radix.Blazor/Html/Binding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Components;
+
+namespace Radix.Blazor.Html
+{
+    public static class bind
+    {
+
+        private const string InputEventName = "input";
+        private const string ChangeEventName = "change";
+
+        public static IAttribute binder<T>(Name name, T currentValue, Action<T> callback, CultureInfo cultureInfo)
+        {
+            return new ExplicitAttribute(
+                name,
+                (builder, sequence, receiver) =>
+                {
+                    builder.AddAttribute(sequence, "on" + name, EventCallback.Factory.CreateBinder(receiver, callback, currentValue, cultureInfo));
+                    return sequence + 1;
+                });
+        }
+
+        public static IAttribute input<T>(T value, Action<T> callback, CultureInfo cultureInfo = default)
+        {
+            return binder(InputEventName, value, callback, cultureInfo);
+        }
+
+        public static IAttribute change<T>(T value, Action<T> callback, CultureInfo cultureInfo = default)
+        {
+            return binder(ChangeEventName, value, callback, cultureInfo);
+        }
+    }
+}
