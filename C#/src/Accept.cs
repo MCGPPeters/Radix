fd5779983ebcbbe3bb14c784c5d8bd9fcec809de
Maurice CGP Peters <maurice.peters@amadeus.com>
2020-05-22 19:14:00 +0200

    Release version of blazor wasm

---
+++ C#/src/Accept.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Radix
+{
+    public delegate Task<Result<TEvent[], Error[]>> Accept<TCommand, TEvent>(Validated<TCommand> validatedCommand) where TCommand : IComparable, IComparable<TCommand>, IEquatable<TCommand>;
+}
