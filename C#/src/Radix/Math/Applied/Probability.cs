68af91030921371d9a5e1c04287155c21b81b1e0
Maurice CGP Peters <mcgppeters@gmail.com>
2020-08-14 09:07:22 +0200

    spiking definitions

---
+++ C#/src/Radix/Math/Applied/Probability.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using static Radix.Validated.Extensions;
+
+namespace Radix.Math.Applied
+{
+    public record Outcome<T>(T Value) : Alias<T>(Value);
+
+    public record Samples<T>(HashSet<T> Value) : Alias<HashSet<T>>(Value);
+    
+    public record Event<T>(Samples<T> sampleSpace, Func<Samples<T>, HashSet<T>> selector) : Alias<HashSet<T>>(selector(sampleSpace));
+
+    public delegate double Probability<T>(Event<T> @event);
+
+
+    namespace Samples
+    {
+        public static class Extensions
+        {
+            public static Probability<T> Probability<T>(this Samples<T> _) => _ => 1d;
+
+            public static Event<T> AsEvent<T>(this Samples<T> samplesSpace) => new Event<T>(samplesSpace, s => { var outcomes = s; return outcomes; });
+        }
+    }
+}
--- C#/src/Radix/Validated/Invalid.cs
+++ C#/src/Radix/Validated/Invalid.cs
@@ -1,20 +1,9 @@
 namespace Radix.Validated
 {
-    public class Invalid<T> : Validated<T>
+    public record Invalid<T>(params string[] Reasons) : Validated<T>
     {
-        internal Invalid(params string[] reasons) => Reasons = reasons;
-
-        public string[] Reasons { get; }
-
         public static implicit operator Invalid<T>(string[] reasons) => new Invalid<T>(reasons);
 
         public static implicit operator string[](Invalid<T> invalid) => invalid.Reasons;
-
-        /// <summary>
-        ///     Type deconstructor, don't remove even though no references are obvious
-        /// </summary>
-        /// <param name="subject"></param>
-        /// <param name="reasons"></param>
-        public void Deconstruct(out string[] reasons) => reasons = Reasons;
     }
 }
