6213ef0c7dd4d0fac02637fd8a53bea129b23c1a
Maurice CGP Peters <mcgppeters@gmail.com>
2020-08-14 09:07:22 +0200

    spiking definitions

--- C#/src/Radix/Alias.cs
+++ C#/src/Radix/Alias.cs
@@ -3,5 +3,7 @@
     public abstract record Alias<T>(T Value)
     {
         public static implicit operator T(Alias<T> alias) => alias.Value;
+
+        public void Deconstruct(out T value) => value = Value;
     }
 }
--- C#/src/Radix/Validated/Valid.cs
+++ C#/src/Radix/Validated/Valid.cs
@@ -3,33 +3,11 @@ using System;
 namespace Radix.Validated
 {
 
-    public class Valid<T> : Validated<T>
+    public record Valid<T>(T Value) : Validated<T>
     {
-        internal Valid(T t)
-        {
-            if (t is object)
-            {
-                Value = t;
-            }
-            else
-            {
-                throw new ArgumentNullException(nameof(t));
-            }
-
-        }
-
-        public T Value { get; }
-
         public static implicit operator Valid<T>(T t) => new Valid<T>(t);
 
         public static implicit operator T(Valid<T> ok) => ok.Value;
-
-
-        /// <summary>
-        ///     Type deconstructor, don't remove even though no references are obvious
-        /// </summary>
-        /// <param name="value"></param>
-        public void Deconstruct(out T value) => value = Value;
     }
 
 }
