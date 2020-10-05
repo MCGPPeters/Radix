5452ca467e8af6be0764b72267d7f9fe8cc74592
Maurice CGP Peters <mcgppeters@gmail.com>
2020-10-05 22:27:42 +0200

    Distribution modad in c#

--- C#/src/Radix/Math/Applied/Probability.cs
+++ C#/src/Radix/Math/Applied/Probability.cs
@@ -1,42 +1,58 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
+using System.Runtime.CompilerServices;
 using static Radix.Validated.Extensions;
 
 namespace Radix.Math.Applied
 {
-    public record Outcome<T>(T Value) : Alias<T>(Value);
+    public record Event<T>(T Value) : Alias<T>(Value);
 
-    /// <summary>
-    /// Since the sample space is a set
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public record SampleSpace<T>(HashSet<T> Value) : Alias<HashSet<T>>(Value);
-
-    /// <summary>
-    /// An event is a subset of the sample space
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public record Event<T>(SampleSpace<T> sampleSpace, Func<SampleSpace<T>, HashSet<T>> selector) : Alias<HashSet<T>>(selector(sampleSpace));
-
-    public delegate double Probability<T>(Event<T> @event);
-    public delegate double Random<T>(SampleSpace<T> sampleSpace);
-
-    namespace Discrete
+    public record Probability : Alias<float>
     {
-        
+        internal Probability(float value) : base(value)
+        {
+
+        }
+
+        public static Func<float, Validated<Probability>> Create =>
+            value =>
+                value switch
+                {
+                    >= 0.0F and <= 1.0F => Valid(new Probability(value)),
+                    _ => Invalid<Probability>("The value of a probability should be in the interval [0.0, 1.0]")
+                };
+
     }
 
-    namespace Samples
+    public delegate float Random<T>(T outcome);
+
+    public record Distribution<T> : Alias<IEnumerable<(Event<T>, Probability)>>
     {
-        public static class Extensions
+        private Distribution(IEnumerable<(Event<T> @event, Probability probability)> distribution) : base(distribution)
+        { }
+
+        public static Distribution<T> Certainly(Event<T> @event) 
+            => new Distribution<T>(Enumerable.Repeat((@event, new Probability(1.0F)), 1));
+
+        public static Distribution<U> Bind<U>(Distribution<T> prior, Func<T, Distribution<U>> f)
         {
-            public static Probability<T> FromSampleSpace<T>(this SampleSpace<T> _) => _ => 1d;
-
-            public static Event<T> AsEvent<T>(this SampleSpace<T> samplesSpace) => new Event<T>(samplesSpace, s => s);
-
-            
+            var result = from Prior in prior.Value
+                         let x = Prior.Item1
+                         let P = Prior.Item2
+                         let posteriors = f(x).Value
+                         from Posterior in posteriors
+                         let y = Posterior.Item1
+                         let Q = Posterior.Item2
+                         select (y, new Probability(P * Q));
+            return new Distribution<U>(result);
         }
+    }
+
+    public static class DistributionExtentions
+    {
+
+        public static Distribution<U> Bind<T, U>(this Distribution<T> prior, Func<T, Distribution<U>> f)
+            => Distribution<T>.Bind(prior, f);
     }
 }
