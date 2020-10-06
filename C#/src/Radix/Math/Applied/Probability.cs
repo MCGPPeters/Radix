60ada57c33977dec08f363be163d84dc0c411649
Maurice CGP Peters <mcgppeters@gmail.com>
2020-10-06 23:05:29 +0200

    Generic shape function for generating distributions

--- C#/src/Radix/Math/Applied/Probability.cs
+++ C#/src/Radix/Math/Applied/Probability.cs
@@ -17,14 +17,14 @@ namespace Radix.Math.Applied
          => Map(e, project);
     }
 
-    public record Probability : Alias<float>
-     {
-        internal Probability(float value) : base(value)
+    public record Probability : Alias<double>
+    {
+        internal Probability(double value) : base(value)
         {
 
         }
 
-        public static Func<float, Validated<Probability>> Create =>
+        public static Func<double, Validated<Probability>> Create =>
             value =>
                 value switch
                 {
@@ -34,28 +34,29 @@ namespace Radix.Math.Applied
 
     }
 
-    public delegate float Random<T>(T outcome);
+    public delegate double Random<T>(T outcome);
+    public delegate Distribution<T> Spread<T>(IEnumerable<T> ts);
 
-    public record Distribution<T> : Alias<IEnumerable<(Event<T>, Probability)>>
+    public record Distribution<T> : Alias<IEnumerable<(Event<T> @event, Probability probability)>>
     {
         private Distribution(IEnumerable<(Event<T> @event, Probability probability)> distribution) : base(distribution)
         { }
 
-        public static Distribution<T> Certainly(Event<T> @event) 
+        public static Distribution<T> Certainly(Event<T> @event)
             => new Distribution<T>(Enumerable.Repeat((@event, new Probability(1.0F)), 1));
 
-        public static Distribution<T> Impossible(Event<T> @event)
-            => new Distribution<T>(Enumerable.Repeat((@event, new Probability(0.0F)), 1));
+        public static Distribution<T> Impossible
+            => new Distribution<T>(Enumerable.Empty<(Event<T>, Probability)>());
 
         internal static Distribution<U> Bind<U>(Distribution<T> prior, Func<T, Distribution<U>> f)
         {
             var result = from Prior in prior.Value
-                         let x = Prior.Item1
-                         let P = Prior.Item2
+                         let x = Prior.@event
+                         let P = Prior.probability
                          let posteriors = f(x).Value
                          from Posterior in posteriors
-                         let y = Posterior.Item1
-                         let Q = Posterior.Item2
+                         let y = Posterior.@event
+                         let Q = Posterior.probability
                          select (y, new Probability(P * Q));
             return new Distribution<U>(result);
         }
@@ -63,12 +64,52 @@ namespace Radix.Math.Applied
         internal static Distribution<U> Map<U>(Distribution<T> distribution, Func<T, U> project)
         {
             var result = from d in distribution.Value
-                         let x = d.Item1
-                         let P = d.Item2
+                         let x = d.@event
+                         let P = d.probability
                          let y = x.Map(project)
                          select (new Event<U>(y), P);
             return new Distribution<U>(result);
         }
+
+        internal static Probability Sum(Distribution<T> distribution)
+        {
+            var result = (from d in distribution.Value
+                          select d.probability.Value).Sum();
+            return new Probability(result);
+        }
+
+        internal static Distribution<T> Scale(Distribution<T> distribution)
+        {
+            var q = Sum(distribution);
+            var d = distribution.Value;
+            return new Distribution<T>(d.Select(x => (x.@event, new Probability(x.probability / q))));
+        }
+
+        public static Spread<T> Shape(Func<double, double> f)
+           =>
+               xs =>
+               {
+
+                   switch (xs.Count())
+                   {
+                       case 0:
+                           return Distribution<T>.Impossible;
+                       default:
+                           var incr = 1.0 / xs.Count() - 1.0;
+                           var probabilities =
+                               _
+                               .Iterate(0.0, x => incr + x)
+                               .Select(f)
+                               .Select(p => new Probability(p));
+                           var events = xs.Select(x => new Event<T>(x));
+                           var d = Enumerable.Zip(events, probabilities, (@event, probability) => (@event, probability));
+                           return Scale(new Distribution<T>(d));
+                   }
+               };
+
+        public static Spread<T> Uniform => Shape(_ => 1.0);
+
+        
     }
 
     public static class DistributionExtentions
@@ -106,7 +147,32 @@ namespace Radix.Math.Applied
         public static Distribution<U> Map<T, U>(this Distribution<T> distribution, Func<T, U> project)
             => Distribution<T>.Map(distribution, project);
 
+        /// <summary>
+        /// for Linq support
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="distribution"></param>
+        /// <param name="project"></param>
+        /// <returns></returns>
         public static Distribution<U> Select<T, U>(this Distribution<T> distribution, Func<T, U> project)
             => distribution.Map(project);
+
+        public static Probability Sum<T, U>(this Distribution<T> distribution)
+            => Distribution<T>.Sum(distribution);
+
+        public static Distribution<T> Scale<T>(this Distribution<T> distribution)
+            => Distribution<T>.Scale(distribution);
+
+
+
+
+
+
     }
+}
+
+
+
+}
 }
