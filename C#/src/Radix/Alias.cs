c91854cafa339284609babe0567514608b172559
Maurice CGP Peters <mcgppeters@gmail.com>
2020-10-06 23:05:29 +0200

    Generic shape function for generating distributions

--- C#/src/Radix/Alias.cs
+++ C#/src/Radix/Alias.cs
@@ -3,7 +3,5 @@
     public abstract record Alias<T>(T Value)
     {
         public static implicit operator T(Alias<T> alias) => alias.Value;
-
-        public void Deconstruct(out T value) => value = Value;
     }
 }
--- C#/src/Radix/Radix.cs
+++ C#/src/Radix/Radix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Radix
 {
@@ -28,6 +29,15 @@ namespace Radix
 
         public static Func<T1, Func<T2, T3, T4, T5, T6, T7, T8, T9, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, T8, T9, R>
             (this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, R> @this) => t1 => (t2, t3, t4, t5, t6, t7, t8, t9) => @this(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+
+        public static IEnumerable<T> Iterate<T>(T seed, Func<T, T> unaryOperator)
+        {
+            while (true)
+            {
+                yield return seed;
+                seed = unaryOperator(seed);
+            }
+        }
     }
 
     public static class _<T>
