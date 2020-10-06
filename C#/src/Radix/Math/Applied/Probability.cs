8a8de1bab936b0dd85689b12b3fec08e070e3219
Maurice CGP Peters <mcgppeters@gmail.com>
2020-10-06 11:06:22 +0200

    Distribution functor map

--- C#/src/Radix/Math/Applied/Probability.cs
+++ C#/src/Radix/Math/Applied/Probability.cs
@@ -8,8 +8,17 @@ namespace Radix.Math.Applied
 {
     public record Event<T>(T Value) : Alias<T>(Value);
 
-    public record Probability : Alias<float>
+    public static class EventExtensions
     {
+        public static Event<U> Map<T, U>(this Event<T> e, Func<T, U> project)
+         => new Event<U>(project(e.Value));
+
+        public static Event<U> Select<T, U>(this Event<T> e, Func<T, U> project)
+         => Map(e, project);
+    }
+
+    public record Probability : Alias<float>
+     {
         internal Probability(float value) : base(value)
         {
 
@@ -35,7 +44,10 @@ namespace Radix.Math.Applied
         public static Distribution<T> Certainly(Event<T> @event) 
             => new Distribution<T>(Enumerable.Repeat((@event, new Probability(1.0F)), 1));
 
-        public static Distribution<U> Bind<U>(Distribution<T> prior, Func<T, Distribution<U>> f)
+        public static Distribution<T> Impossible(Event<T> @event)
+            => new Distribution<T>(Enumerable.Repeat((@event, new Probability(0.0F)), 1));
+
+        internal static Distribution<U> Bind<U>(Distribution<T> prior, Func<T, Distribution<U>> f)
         {
             var result = from Prior in prior.Value
                          let x = Prior.Item1
@@ -47,12 +59,54 @@ namespace Radix.Math.Applied
                          select (y, new Probability(P * Q));
             return new Distribution<U>(result);
         }
+
+        internal static Distribution<U> Map<U>(Distribution<T> distribution, Func<T, U> project)
+        {
+            var result = from d in distribution.Value
+                         let x = d.Item1
+                         let P = d.Item2
+                         let y = x.Map(project)
+                         select (new Event<U>(y), P);
+            return new Distribution<U>(result);
+        }
     }
 
     public static class DistributionExtentions
     {
-
+        /// <summary>
+        /// Monadic bind
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="prior"></param>
+        /// <param name="f"></param>
+        /// <returns></returns>
         public static Distribution<U> Bind<T, U>(this Distribution<T> prior, Func<T, Distribution<U>> f)
             => Distribution<T>.Bind(prior, f);
+
+        /// <summary>
+        /// For Linq support
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="prior"></param>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public static Distribution<U> SelectMany<T, U>(this Distribution<T> prior, Func<T, Distribution<U>> f)
+            => prior.Bind(f);
+
+        /// <summary>
+        /// Functor map
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="distribution"></param>
+        /// <param name="project"></param>
+        /// <returns></returns>
+        public static Distribution<U> Map<T, U>(this Distribution<T> distribution, Func<T, U> project)
+            => Distribution<T>.Map(distribution, project);
+
+        public static Distribution<U> Select<T, U>(this Distribution<T> distribution, Func<T, U> project)
+            => distribution.Map(project);
     }
 }
