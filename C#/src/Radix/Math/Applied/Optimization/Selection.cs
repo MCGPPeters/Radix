93ace7dba673fec0a4f2d12da15ffa53eedb0b90
Maurice CGP Peters <mcgppeters@gmail.com>
2020-10-08 17:27:09 +0200

    ArgMax alias

---
+++ C#/src/Radix/Math/Applied/Optimization/Selection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Radix.Collections.Generic.Enumerable;
+
+namespace Radix.Math.Applied.Optimization
+{
+    public static class Selection
+    {
+        public static Func<Func<T, U>, IEnumerable<T>, T> ArgMax<T, U>() => (f, xs) => xs.MaxBy(f);
+    }
+}
