01a4ae933fc0b9b86267800e6493908b1c839c92
Maurice CGP Peters <mcgppeters@gmail.com>
2021-02-04 13:21:00 +0100

    Real numbers as fields

---
+++ C#/src/Radix/Math/Pure/Numbers/ℚ/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using static Radix.Math.Pure.Numbers.ℤ.Extensions;
+
+namespace Radix.Math.Pure.Numbers.ℚ
+{
+    public static class Extensions
+    {
+        private static Func<Rational, Result<Rational, Error>> Reduce =>
+            (x) =>
+            {
+                var d = Gcd(x.Numerator, x.Denominator);
+                return Rational.Create(x.Numerator % d, x.Denominator % d);
+            };
+    }
+}
