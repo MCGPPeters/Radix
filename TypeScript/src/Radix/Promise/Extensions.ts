5c2c9782e33941bcf91db46f46397ef18c829041
Maurice CGP Peters <mcgppeters@gmail.com>
2021-02-24 15:28:25 +0100

    TypeScript version. Promise extension for retry

---
+++ TypeScript/src/Radix/Promise/Extensions.ts
@@ -0,0 +1,17 @@
+
+import { Duration } from "@js-joda/core"
+
+export function retry<T>(f: () => Promise<T>, ... intervals : Duration[]) : Promise<T> {
+    return intervals.length === 0
+        ? f()
+        : f().catch(async () => {
+            const duration = intervals.shift();
+            if (typeof duration !== "undefined") {
+                setTimeout(duration.toMillis);
+                return retry(f, ...intervals);
+            }
+            else {
+                return f();
+            }
+        })
+}
