171c152fdac2ed466cf6239545d70c3e638ce674
Maurice CGP Peters <mcgppeters@gmail.com>
2021-04-02 11:30:02 +0200

    option and result

---
+++ TypeScript/src/Radix/Option.ts
@@ -0,0 +1,30 @@
+export default Option
+
+export type None = {
+    tag: "none"
+};
+
+export type Some<T> = {
+    tag: "some"
+    value: T
+}
+
+export function Some<T>(t: T): Some<T> { return { tag: "some", value: t} }
+
+export const None : None = {tag: "none"};
+
+export type Option<T> = Some<T> | None
+
+export function bind<T, U>(option: Option<T>, f: (t: T) => Option<U>): Option<U> {
+    switch (option.tag) {
+        case "some" : return f(option.value)
+        case "none" : return None
+    }
+}
+
+export function map<T, U>(option: Option<T>, f: (t: T) => U): Option<U>{
+    switch (option.tag) {
+        case "some" : return Some<U>(f(option.value))
+        case "none" : return None
+    }
+}
--- TypeScript/src/Radix/index.ts
+++ TypeScript/src/Radix/index.ts
@@ -1,1 +1,4 @@
 export * from "./Promise/Extensions";
+export * as Writer from "./Writer";
+export * as Result from "./Result";
+export * as Option from "./Option";
