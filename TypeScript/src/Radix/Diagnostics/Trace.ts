33ddef6de1227356b29a98b98b3fc72d99ffde10
Maurice CGP Peters <mcgppeters@gmail.com>
2021-04-17 20:00:52 +0200

    generic trace

--- TypeScript/src/Radix/Diagnostics/Trace.ts
+++ TypeScript/src/Radix/Diagnostics/Trace.ts
@@ -8,18 +8,12 @@ export type Call<T, TError> = {
     // detailed information on the call, such as the url or the SQL statement
     context: string
     // translate the
-    getResultCode: (t: Result.Result<T, TError>) => string
+    getResultCode: (result: Result.Result<T, TError>) => string
 }
 
 export type Traceable<T, U, Error> = (t: T) => PromiseResult.PromiseResult<U, Error>
 
 export type SendTrace<T, TError> = (result: Result.Result<T, TError>, call: Call<T, TError>, duration: number) => void
-
-export const createSendTrace = <T, TError>(telemetryClient: appInsights.TelemetryClient) : SendTrace<T, TError> =>{
-    return (result: Result.Result<T, TError>, call: Call<T, TError>, duration: number) => {
-        return telemetryClient.trackDependency({dependencyTypeName: call.type, data: call.context, duration: duration, name: call.name, success: result.tag === "ok", resultCode: call.getResultCode(result)})
-    }
-}
 
 export const trace = <T, U, TError> (sendTrace: SendTrace<U, TError>) => (f: Traceable<T, U, TError>, call: Call<U, TError>): Traceable<T, U, TError> => {
     return async (t: T) => {
