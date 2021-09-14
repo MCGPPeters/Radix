d05f2d5b81e9f081393e0e41fcb7fc964d7ba992
Maurice Peters <mcgppeters@gmail.com>
2021-09-14 10:14:53 +0200

    feat: Convenience overloads for less clutter and better format
    Uses a source generator for all html element methods

--- C#/src/Samples/Inventory/Radix.Inventory/Pages/AddInventoryItemComponent.cs
+++ C#/src/Samples/Inventory/Radix.Inventory/Pages/AddInventoryItemComponent.cs
@@ -5,93 +5,193 @@ using Radix.Components;
 using Radix.Components.Html;
 using Radix.Inventory.Domain;
 using Radix.Option;
-using static Radix.Components.Html.Attributes;
-using static Radix.Components.Html.Components;
-using static Radix.Components.Html.Elements;
 
 namespace Radix.Blazor.Inventory.Server.Pages;
 
 [Route("/Add")]
 public class AddInventoryItemComponent : TaskBasedComponent<AddInventoryItemViewModel, InventoryItemCommand, InventoryItemEvent, Json>
 {
-    protected override Node View(AddInventoryItemViewModel currentViewModel) => concat(
-        h1(Attributes.None, text("Add new item")),
-        div(
-            new[] { @class("form-group") },
-            Elements.label(
-                new[] { @for("idInput") },
-                text("Id")),
-            input(
-                @class("form-control"),
-                id("idInput"),
-                bind.input(currentViewModel.InventoryItemId, id => currentViewModel.InventoryItemId = id)),
-            Elements.label(
-                new[] { @for("nameInput") },
-                text("Name")),
-            input(
-                @class("form-control"),
-                id("nameInput"),
-                bind.input(currentViewModel.InventoryItemName, name => currentViewModel.InventoryItemName = name)),
-            Elements.label(
-                new[] { @for("countInput") },
-                text("Count")),
-            input(
-                @class("form-control"),
-                id("countInput"),
-                bind.input(currentViewModel.InventoryItemCount, count => currentViewModel.InventoryItemCount = count)
-            )),
-        button(
-            new[]
-            {
-                    @class("btn btn-primary"), on.click(
-                        async args =>
-                        {
-                            Validated<InventoryItemCommand> validCommand = CreateInventoryItem.Create(
-                                currentViewModel.InventoryItemId,
-                                currentViewModel.InventoryItemName,
-                                true,
-                                currentViewModel.InventoryItemCount);
-
-                            var inventoryItem = BoundedContext.Create<InventoryItem, InventoryItemCommandHandler>();
-                            Option<Error[]> result = await Dispatch(inventoryItem, validCommand);
-                            switch (result)
+    protected override Node View(AddInventoryItemViewModel currentViewModel) =>
+        concat
+        (
+            h1
+            (
+                text
+                (
+                    "Add new item")
+                ),
+            div
+            (
+                new[] { @class("form-group") },
+                label
+                (
+                    new[] { @for("idInput") },
+                    text
+                    (
+                        "Id"
+                    )
+                ),
+                input
+                (
+                    @class("form-control"),
+                    id("idInput"),
+                    bind.input(currentViewModel.InventoryItemId, id => currentViewModel.InventoryItemId = id)
+                ),
+                label
+                (
+                    new[] { @for("nameInput") },
+                    text("Name")
+                ),
+                input
+                (
+                    @class("form-control"),
+                    id("nameInput"),
+                    bind.input(currentViewModel.InventoryItemName, name => currentViewModel.InventoryItemName = name)
+                ),
+                label
+                (
+                    new[] { @for("countInput") },
+                    text
+                    (
+                        "Count"
+                    )
+                ),
+                input
+                (
+                    @class("form-control"),
+                    id("countInput"),
+                    bind.input(currentViewModel.InventoryItemCount, count => currentViewModel.InventoryItemCount = count)
+                )
+            ),
+            button
+            (
+                new[]
+                {
+                        @class("btn btn-primary"),
+                        on.click
+                        (
+                            async args =>
                             {
-                                case Some<Error[]>(_):
-                                    if (JSRuntime is not null)
-                                    {
-                                        await JSRuntime.InvokeAsync<string>("toast", Array.Empty<object>());
-                                    }
+                                Validated<InventoryItemCommand> validCommand = CreateInventoryItem.Create(
+                                    currentViewModel.InventoryItemId,
+                                    currentViewModel.InventoryItemName,
+                                    true,
+                                    currentViewModel.InventoryItemCount);
 
-                                    break;
-                                case None<Error[]> _:
-                                    NavigationManager.NavigateTo("/");
-                                    break;
-                            }
-                        })
-            },
-            text("Ok")
-        ),
-        navLinkMatchAll(new[] { @class("btn btn-primary"), href("/") }, text("Cancel")),
-        div(
-            Attributes.None,
-            div(
-                new[] { @class("toast"), attribute("data-autohide", "false") },
-                div(
-                    new[] { @class("toast-header") },
-                    strong(new[] { @class("mr-auto") }, text("Invalid input")),
-                    small(Attributes.None, text(DateTimeOffset.UtcNow.ToString(CultureInfo.CurrentUICulture))),
-                    button(new[] { type("button"), @class("ml-2 mb-1 close"), attribute("data-dismiss", "toast") }, Elements.span(Attributes.None, text("🗙")))),
-                div(
-                    new[] { @class("toast-body") },
-                    FormatErrorMessages(currentViewModel.Errors)
-                ))));
+                                var inventoryItem = BoundedContext.Create<InventoryItem, InventoryItemCommandHandler>();
+                                Option<Error[]> result = await Dispatch(inventoryItem, validCommand);
+                                switch (result)
+                                {
+                                    case Some<Error[]>(_):
+                                        if (JSRuntime is not null)
+                                        {
+                                            await JSRuntime.InvokeAsync<string>("toast", Array.Empty<object>());
+                                        }
+
+                                        break;
+                                    case None<Error[]> _:
+                                        NavigationManager.NavigateTo("/");
+                                        break;
+                                }
+                            })
+                },
+                text
+                (
+                    "Ok"
+                )
+            ),
+            navLinkMatchAll
+            (
+                new[]
+                {
+                    @class("btn btn-primary"),
+                    href("/")
+                },
+                text
+                (
+                    "Cancel"
+                )
+            ),
+            div
+            (
+                div
+                (
+                    new[]
+                    {
+                        @class("toast"),
+                        attribute("data-autohide", "false")
+                    },
+                    div
+                    (
+                        new[]
+                        {
+                            @class("toast-header")
+                        },
+                        strong
+                        (
+                            new[]
+                            {
+                                @class("mr-auto")
+                            },
+                            text
+                            (
+                                "Invalid input"
+                                )
+                            ),
+                            small
+                            (
+                                text
+                                (
+                                    DateTimeOffset.UtcNow.ToString(CultureInfo.CurrentUICulture)
+                                )
+                            ),
+                            button
+                            (
+                                new[]
+                                {
+                                    type("button"),
+                                    @class("ml-2 mb-1 close"),
+                                    attribute("data-dismiss", "toast")
+                                },
+                                span
+                                (
+                                    text
+                                    (
+                                        "🗙"
+                                    )
+                                )
+                            )
+                        ),
+                        div
+                        (
+                            new[]
+                            {
+                                @class("toast-body")
+                            },
+                            FormatErrorMessages(currentViewModel.Errors)
+                        )
+                    )
+                )
+            );
 
     private static Node FormatErrorMessages(IEnumerable<Error> errors)
     {
         Node node = new Empty();
         if (errors is not null)
         {
-            node = ul(Array.Empty<IAttribute>(), errors.Select(error => li(Array.Empty<IAttribute>(), text(error.ToString()))).ToArray());
+            node =
+                ul
+                (
+                    errors.Select(error =>
+                        li
+                        (
+                            text
+                            (
+                                error.ToString()
+                            )
+                        )
+                    ).ToArray()
+                );
         }
 
         return node;
