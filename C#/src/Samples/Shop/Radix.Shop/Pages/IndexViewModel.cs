5d70ff74dcea15a557c26e3e6bf0fcfaf74c5e42
Maurice Peters <mcgppeters@gmail.com>
2021-09-17 17:41:59 +0200

    wip:[Sample] Index Setup

---
+++ C#/src/Samples/Shop/Radix.Shop/Pages/IndexViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Radix.Components;
+
+namespace Radix.Shop.Pages
+{
+    public record IndexViewModel : ViewModel
+    {
+        public IndexViewModel()
+        {
+            Types = new List<Type>()
+            {
+                "Mug",
+                "Sheet",
+                "T-Shirt"
+            };
+
+            Brands = new List<Brand>()
+            {
+                ".NET",
+                "Azure",
+                "Other"
+            };
+        }
+        public string TypeFilter { get; internal set; }
+        public string BrandFilter { get; internal set; }
+        public IEnumerable<Product> FilteredProducts { get; internal set; }
+        public IEnumerable<Type> Types { get; internal set; }
+        public IEnumerable<Brand> Brands { get; internal set; } 
+
+        internal Action<MouseEventArgs> ApplyFilter() => _ => { };
+    }
+}
