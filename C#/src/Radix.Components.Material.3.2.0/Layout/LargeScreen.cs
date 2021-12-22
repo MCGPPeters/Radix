0e8c78325fcddbe9f13a96661e8e8dcd3b7351d7
Maurice Peters <mcgppeters@gmail.com>
2021-12-22 13:11:27 +0100

    setup

---
+++ C#/src/Radix.Components.Material.3.2.0/Layout/LargeScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Radix.Components.Html;
+
+namespace Radix.Components.Material._3._2._0.Layout
+{
+    public class LargeScreen : Component<LargeScreenViewModel>
+    {
+        protected override Node View(LargeScreenViewModel currentViewModel) => throw new NotImplementedException();
+    }
+
+    public record LargeScreenViewModel : ViewModel
+    {
+
+    }
+}
