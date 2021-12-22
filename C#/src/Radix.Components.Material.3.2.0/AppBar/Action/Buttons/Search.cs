b9b6757088c19718397785acd1cca347d4d2c3ad
Maurice CGP Peters <mcgppeters@gmail.com>
2021-12-22 17:19:45 +0100

    feat: material regular component with subcomponents

---
+++ C#/src/Radix.Components.Material.3.2.0/AppBar/Action/Buttons/Search.cs
@@ -0,0 +1,10 @@
+﻿using Radix.Components.Html;
+
+namespace Radix.Components.Material._3._2._0.AppBar.Action.Buttons;
+
+public class Search : Button
+{
+    protected override string Name => "search";
+
+    protected override string AriaLabel => "Search";
+}
