d8d3d75ed24d4a45a20b468a0052d69dbc9b016e
Maurice CGP Peters <mcgppeters@gmail.com>
2022-01-07 15:10:56 +0100

    fix: reserved names should have verbatim identifiers

---
+++ C#/src/Radix.Components/Html/Names/Attributes.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radix.Components.Html.Names.Attributes
+{
+    // https://html.spec.whatwg.org/multipage/indices.html#index
+
+    [Literal]
+    public partial struct abbr { }
+
+    [Literal]
+    public partial struct accept { }
+
+    [Literal(StringRepresentation = "accept-charset")]
+    public partial struct acceptCharset { }
+
+    [Literal]
+    public partial struct accesskey { }
+
+    [Literal]
+    public partial struct action { }
+
+    [Literal]
+    public partial struct allow { }
+
+    [Literal]
+    public partial struct allowfullscreen { }
+
+    [Literal]
+    public partial struct alt { }
+
+    [Literal]
+    public partial struct async { }
+
+    [Literal]
+    public partial struct autocapitalize { }
+
+    [Literal]
+    public partial struct autocomplete { }
+
+    [Literal]
+    public partial struct autofocus { }
+
+    [Literal]
+    public partial struct autoplay { }
+
+    [Literal]
+    public partial struct charset { }
+
+    [Literal(StringRepresentation = "checked")]
+    public partial struct @checked { }
+
+    [Literal]
+    public partial struct cite { }
+
+    [Literal(StringRepresentation = "class")]
+    public partial struct @class { }
+
+    [Literal]
+    public partial struct color { }
+
+    [Literal]
+    public partial struct cols { }
+
+    [Literal]
+    public partial struct colspan { }
+
+    [Literal]
+    public partial struct content { }
+
+    [Literal]
+    public partial struct contenteditable { }
+
+    [Literal]
+    public partial struct controls { }
+
+    [Literal]
+    public partial struct coords { }
+
+    [Literal]
+    public partial struct crossorigin { }
+
+    [Literal]
+    public partial struct data { }
+
+    [Literal]
+    public partial struct datetime { }
+
+    [Literal]
+    public partial struct decoding { }
+
+    [Literal(StringRepresentation = "default")]
+    public partial struct @default { }
+
+    [Literal]
+    public partial struct defer { }
+
+    [Literal]
+    public partial struct dir { }
+
+    [Literal]
+    public partial struct dirname { }
+
+    [Literal]
+    public partial struct disabled { }
+
+    [Literal]
+    public partial struct download { }
+
+    [Literal]
+    public partial struct draggable { }
+
+    [Literal]
+    public partial struct enctype { }
+
+    [Literal]
+    public partial struct enterkeyhint { }
+
+    [Literal(StringRepresentation = "for")]
+    public partial struct @for { }
+
+    [Literal]
+    public partial struct form { }
+
+    [Literal]
+    public partial struct formaction { }
+
+    [Literal]
+    public partial struct formenctype { }
+
+    [Literal]
+    public partial struct formmethod { }
+
+    [Literal]
+    public partial struct formnovalidate { }
+
+    [Literal]
+    public partial struct formtarget { }
+
+    [Literal]
+    public partial struct headers { }
+
+    [Literal]
+    public partial struct height { }
+
+    [Literal]
+    public partial struct hidden { }
+
+    [Literal]
+    public partial struct high { }
+
+    [Literal]
+    public partial struct href { }
+
+    [Literal]
+    public partial struct hreflang { }
+
+    [Literal(StringRepresentation = "http-equiv")]
+    public partial struct httpEquiv { }
+
+    [Literal]
+    public partial struct id { }
+
+    [Literal]
+    public partial struct imagesizes { }
+
+    [Literal]
+    public partial struct imagesrcset { }
+
+    [Literal]
+
+    public partial struct inputmode { }
+
+    [Literal(StringRepresentation = "is")]
+    public partial struct @is { }
+
+    [Literal]
+    public partial struct ismap { }
+
+    [Literal]
+    public partial struct itemid { }
+
+    [Literal]
+    public partial struct itemprop { }
+
+    [Literal]
+    public partial struct itemref { }
+
+    [Literal]
+    public partial struct itemscope { }
+
+    [Literal]
+    public partial struct itemtype { }
+
+    [Literal]
+    public partial struct kind { }
+
+    [Literal]
+    public partial struct label { }
+
+    [Literal]
+    public partial struct lang { }
+
+    [Literal]
+    public partial struct list { }
+
+    [Literal]
+    public partial struct loading { }
+
+    [Literal]
+    public partial struct loop { }
+
+    [Literal]
+    public partial struct low { }
+
+    [Literal]
+    public partial struct max { }
+    [Literal]
+    public partial struct maxlength { }
+
+    [Literal]
+    public partial struct media { }
+
+    [Literal]
+    public partial struct method { }
+
+    [Literal]
+    public partial struct min { }
+
+    [Literal]
+    public partial struct minlength { }
+
+    [Literal]
+    public partial struct multiple { }
+    [Literal]
+    public partial struct muted { }
+    [Literal]
+    public partial struct name { }
+
+    [Literal]
+    public partial struct nomodule { }
+
+    [Literal]
+    public partial struct nonce { }
+
+    [Literal]
+    public partial struct novalidate { }
+
+    [Literal]
+    public partial struct open { }
+
+    [Literal]
+    public partial struct optimum { }
+
+    [Literal]
+    public partial struct pattern { }
+
+    [Literal]
+    public partial struct ping { }
+
+    [Literal]
+    public partial struct placeholder { }
+
+    [Literal]
+    public partial struct playsinline { }
+
+    [Literal]
+    public partial struct poster { }
+    [Literal]
+    public partial struct preload { }
+
+    [Literal(StringRepresentation = "readonly")]
+    public partial struct @readonly { }
+
+    [Literal]
+    public partial struct referrerpolicy { }
+
+    [Literal]
+    public partial struct rel { }
+
+    [Literal]
+    public partial struct required { }
+
+    [Literal]
+    public partial struct reversed { }
+
+    [Literal]
+    public partial struct rows { }
+
+    [Literal]
+    public partial struct rowspan { }
+
+    [Literal]
+    public partial struct sandbox { }
+
+    [Literal]
+    public partial struct scope { }
+
+
+    public partial struct selected { }
+
+    [Literal]
+    public partial struct shape { }
+
+    [Literal]
+    public partial struct size { }
+
+    [Literal]
+    public partial struct sizes { }
+
+    [Literal]
+    public partial struct slot { }
+
+    [Literal]
+    public partial struct span { }
+
+    [Literal]
+    public partial struct spellcheck { }
+
+    [Literal]
+    public partial struct src { }
+
+    [Literal]
+    public partial struct srcdoc { }
+
+    [Literal]
+    public partial struct srclang { }
+
+    [Literal]
+    public partial struct srcset { }
+
+    [Literal]
+    public partial struct start { }
+
+    [Literal]
+    public partial struct step { }
+
+    [Literal]
+    public partial struct stÿle { }
+
+    [Literal]
+    public partial struct tabindex { }
+
+    [Literal]
+    public partial struct target { }
+
+    [Literal]
+    public partial struct title { }
+
+    [Literal]
+    public partial struct translate { }
+
+    [Literal]
+    public partial struct type { }
+
+    [Literal]
+    public partial struct usemap { }
+
+    [Literal]
+    public partial struct value { }
+
+    [Literal]
+    public partial struct width { }
+
+    [Literal]
+    public partial struct wrap { }
+
+}
