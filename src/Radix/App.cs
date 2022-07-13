1ab2614d7d25954646f680d33bd64581a40feba1
Maurice CGP Peters <mcgppeters@gmail.com>
2022-07-13 16:15:42 +0200

    latest nugets

--- src/Radix/App.cs
+++ src/Radix/App.cs
@@ -1,26 +1,26 @@
-﻿namespace Radix;
+﻿//namespace Radix;
 
-public interface Type<T>
-{
-    static abstract Type<T> Inject(T t);
-    T Project();
-}
+//public interface Type<T>
+//{
+//    static abstract Type<T> Inject(T t);
+//    T Project();
+//}
 
-public interface Functor<K, T>
-    where K : Type<T>
-{
-    static abstract Type<R> Map<R>(K type, Func<T, R> f);
-}
+//public interface Functor<K, T>
+//    where K : Type<T>
+//{
+//    static abstract Type<R> Map<R>(K type, Func<T, R> f);
+//}
 
-public interface Applicative<K, T> : Functor<K, T>
-    where K : Type<T>
-{
-    static abstract Type<T> Return(T t);
-}
+//public interface Applicative<K, T> : Functor<K, T>
+//    where K : Type<T>
+//{
+//    static abstract Type<T> Return(T t);
+//}
 
-public interface Monad<K, T> :
-    Applicative<K, T>
-    where K : Type<T>
-{
-    static abstract Type<R> Bind<KR, R>(K type, Func<T, Type<R>> f);
-}
+//public interface Monad<K, T> :
+//    Applicative<K, T>
+//    where K : Type<T>
+//{
+//    static abstract Type<R> Bind<KR, R>(K type, Func<T, Type<R>> f);
+//}
